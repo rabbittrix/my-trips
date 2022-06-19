355f79627c3f18c575a44ac65ebf8af949fef776
rabbittrix <rabbittrix@hotmail.com>
2022-06-19 13:30:30 +0100

    Refacturing same code

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://github.com/rabbittrix/my-trips',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@Roberto de Souza',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
